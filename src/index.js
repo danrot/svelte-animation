d519fbdc31f92f6e4cedcc6ac3d855c2fb2d794c
Daniel Rotter <daniel.rotter@gmail.com>
2021-08-06 15:08:33 +0200

    Initialize svelte

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Test');
+import App from './App.svelte';
+
+const app = new App({target: document.body});
+
+export default app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: 'svelte-loader',
+                    options: {
+                        emitCss: true,
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
     plugins: [
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             title: "Svelte Animation",
         }),
-    ]
+    ],
 };
